08bc2503a760a837c4d51486ffe81b80de78bf48
klogan2594 <klogan2594@gmail.com>
2019-10-20 18:16:34 -0500

    basic message parsing

--- bot.js
+++ bot.js
@@ -7,9 +7,31 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
-    }
+  parseReply(msg);
 });
+
+
+function parseReply(msg) {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  } else if (msg.content === 'Is a hotdog a sandwich?') {
+    msg.reply('Of course it is, you filthy casual.');
+  } else if (msg.content.includes('!schedule')) {
+    scheduleEvent(msg);
+  }
+}
+
+
+function scheduleEvent(msg) {
+  //msg.reply("http://gph.is/1BIJbcc");
+  msg.channel.send('its working!')
+
+  const parsedContent = msg.content.split(" ")
+  console.log(parsedContent.toString());
+
+  
+
+}
+
 
 client.login(auth.token);

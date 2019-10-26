43e249049b16a85b1039f61750282aa9969e6865
klogan2594 <klogan2594@gmail.com>
2019-10-26 18:58:35 -0500

    now monitoring event messages for reacts

--- bot.js
+++ bot.js
@@ -2,23 +2,70 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
 const config = require('./config.json');
+const assert = require('assert');
+var cache = new Map();
 
 
-//query to get eventIds
-//db.events.find({"eventMsgId":{$ne:null}}, {eventMsgId:1, _id:0 });
-
-
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("Doubling down on being bold.");
+  client.user.setActivity("Bold Simulator 2020");
+  getEventMsgIds()
+});
+
+
+
+//Need to run this chunk to listen for reactions on non-cached messages
+client.on('raw', packet => {
+  // We don't want this to run on unrelated packets
+  if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
+  // Grab the channel to check the message from
+  const channel = client.channels.get(packet.d.channel_id);
+  // There's no need to emit if the message is cached, because the event will fire anyway for that
+  if (channel.messages.has(packet.d.message_id)) return;
+  // Since we have confirmed the message is not cached, let's fetch it
+  channel.fetchMessage(packet.d.message_id).then(message => {
+      // Emojis can have identifiers of name:id format, so we have to account for that case as well
+      const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
+      // This gives us the reaction we need to emit the event properly, in top of the message object
+      const reaction = message.reactions.get(emoji);
+      // Adds the currently reacting user to the reaction's users collection.
+      if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
+      // Check which type of event it is before emitting
+      if (packet.t === 'MESSAGE_REACTION_ADD') {
+          client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
+      }
+      if (packet.t === 'MESSAGE_REACTION_REMOVE') {
+          client.emit('messageReactionRemove', reaction, client.users.get(packet.d.user_id));
+      }
+  });
 });
 
 
 client.on('messageReactionAdd', (messageReaction, user) => {
-  console.log('hellooooooo');
-  console.log(messageReaction.message.id);
-  console.log(user.id + ": " + user.username);
-}); 
+  //console.dir(messageReaction);
+  //console.dir(user);
+  let msgId = messageReaction.message.id;
+  let eventMsgIds = cache.get("eventMsgIds");
+  let reactEmoji = messageReaction.emoji.name;
+  //console.log('hellooooooo');
+  //console.log(messageReaction.message.id);
+  //console.log(messageReaction.emoji.name);
+  //console.log(user.id + ": " + user.username);
+  //console.dir(cache);
+
+  if (eventMsgIds.includes(msgId) && reactEmoji === '✅') {
+    console.log("its working!")
+    //Get the info we need to add the user to the event
+    let msg = messageReaction.message;
+    let msgText = msg.content;
+    let reactingUserName = user.username;
+    let reactingUserId = user.id;
+
+    let newMsg = msgText += `, ${reactingUserName}`
+    msg.edit(newMsg); 
+
+  }
+});
 
 
 client.on('message', async msg => {
@@ -38,8 +85,8 @@ client.on('message', async msg => {
   if (msg.content.includes('schedule')) {
     //example request -> !boldBot schedule destiny raid 10/25/19 7:00pm
     let parsedContent = msg.content.split(" ");
-    console.log(parsedContent.toString());
-    console.log(msg.author.id);
+    //console.log(parsedContent.toString());
+    //console.log(msg.author.id);
 
     if (parsedContent.length != 6) {
       //msg.author.send('u r dum');
@@ -49,31 +96,27 @@ client.on('message', async msg => {
 
     var [prefix, action, game, gameMode, date, time] = parsedContent;
     var creatorId = msg.author.id;
+    var creatorUsername = msg.author.username;
     var eventMsgId;
 
     //store event to mongodb
-    var MongoClient = require('mongodb').MongoClient,
-      f = require('util').format,
-      assert = require('assert');
-
-    var user = encodeURIComponent(auth.mongoUser);
-    var password = encodeURIComponent(auth.mongoPw);
-    var authMechanism = 'DEFAULT';
-
-    // Connection URL
-    var url = f('mongodb://%s:%s@localhost:27017/boldBotDB?authMechanism=%s',
-      user, password, authMechanism);
+    const {MongoClient, url} = mongoInit();
 
     // Use connect method to connect to the Server
     MongoClient.connect(url, async function (err, db) {
       assert.equal(null, err);
-      console.log("Connected correctly to server");
+      //console.log("Connected correctly to server");
       if (err) throw err;
 
       //send response and get response message id
-      let eventMsg = await msg.channel.send(`Got it. ${game} ${gameMode} scheduled for ${date} at ${time}.`)
+      let eventMsg = await msg.channel.send(`Got it. ${game} ${gameMode} scheduled for ${date} at ${time}. \n To join the event, react with a ✅'. \n Current Participants: ${creatorUsername}`)
       eventMsgId = eventMsg.id;
       console.log("eventMsgId: " + eventMsgId);
+
+      //store new msgId to cache
+      let curCache = cache.get("eventMsgIds") || [];
+      curCache.push(eventMsgId);
+      cache.set('eventMsgIds', curCache);
 
       //generate JSON
       //TODO: store date and time as JS date object
@@ -88,18 +131,14 @@ client.on('message', async msg => {
       };
 
       console.dir(eventBlob);
-      
+
       var dbo = db.db("boldBotDB");
       dbo.collection("events").insertOne(eventBlob, function (err, res) {
         if (err) throw err;
         console.log("1 document inserted");
         db.close();
       });
-      
     });
-
-
-
   }
 
   //TODO: Don't grab events that have already happened.
@@ -107,24 +146,13 @@ client.on('message', async msg => {
     var msgString = "**List of events incoming:** \n\n";
     var results;
 
-
     //Get stuff ready to query DB
-    var MongoClient = require('mongodb').MongoClient,
-      f = require('util').format,
-      assert = require('assert');
-
-    var user = encodeURIComponent(auth.mongoUser);
-    var password = encodeURIComponent(auth.mongoPw);
-    var authMechanism = 'DEFAULT';
-
-    // Connection URL
-    var url = f('mongodb://%s:%s@localhost:27017/boldBotDB?authMechanism=%s',
-      user, password, authMechanism);
+    const {MongoClient, url} = mongoInit();
 
     // Use connect method to connect to the Server
     await MongoClient.connect(url, async function (err, db) {
       assert.equal(null, err);
-      console.log("Connected correctly to server");
+      //console.log("Connected correctly to server");
 
       if (err) throw err;
       var dbo = db.db("boldBotDB");
@@ -143,3 +171,46 @@ client.on('message', async msg => {
 
 
 client.login(auth.token);
+
+
+async function getEventMsgIds() {
+  //Get stuff ready to query DB
+  const {MongoClient, url} = mongoInit();
+
+  // Use connect method to connect to the Server
+  MongoClient.connect(url, function (err, db) {
+    assert.equal(null, err);
+    //console.log("Connected correctly to server");
+
+    if (err) throw err;
+    var dbo = db.db("boldBotDB");
+
+    dbo.collection("events").find({}).project({ eventMsgId: 1, _id: 0 }).toArray(function (err, result) {
+      if (err) throw err;
+      //console.log(result.eventMsgId);
+      var parsedResults = result.map(event => event.eventMsgId);
+      //console.log(parsedResults);
+      db.close();
+      cache.set('eventMsgIds', parsedResults);
+      console.log("Event Message Ids cached.");
+    });
+  });
+}
+
+
+function mongoInit() {
+  var MongoClient = require('mongodb').MongoClient;
+  var f = require('util').format;
+  var user = encodeURIComponent(auth.mongoUser);
+  var password = encodeURIComponent(auth.mongoPw);
+  var authMechanism = 'DEFAULT';
+  // Connection URL
+  var url = f('mongodb://%s:%s@localhost:27017/boldBotDB?authMechanism=%s',
+    user, password, authMechanism);
+  return {MongoClient: MongoClient, url: url};
+}
+
+
+function createReactionCollectors() {
+
+}
